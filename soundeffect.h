8d723f0c9e305bfbdb259abc256a37903e3804aa
Jatlus <arj.sarkar10@gmail.com>
2024-04-22 15:55:19 -0600

    added comments for digscene.h, dinosaurbone.h, dinosaurname.h, keystroke.h, scene.h and soundeffect.h

--- soundeffect.h
+++ soundeffect.h
@@ -1,6 +1,10 @@
 #ifndef SOUNDEFFECT_H
 #define SOUNDEFFECT_H
 
+/// @brief Header file for soundeffects
+/// Used to access various sound effects
+/// Reviewed by Arjun Sarkar
+
 enum class SoundEffect{
     digSpot,
     walk,
