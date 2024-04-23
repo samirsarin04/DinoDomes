a0c188a9e845dca1f920fcbcf080e365a201a49f
peytjensen <peytonejensen@gmail.com>
2024-04-23 17:38:09 -0600

    comment view.h

--- view.h
+++ view.h
@@ -7,46 +7,67 @@
 #include "QPainter"
 #include "soundeffect.h"
 #include <QSoundEffect>
-//#include <QMediaPlayer>
 #include <QtMultimedia>
-
-
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class View;
 }
 QT_END_NAMESPACE
-
+///@brief view class declaration that is used for the dinodomes game
+///Reviewed by Peyton Jensen
 class View : public QMainWindow
 {
     Q_OBJECT
 
 protected:
+    ///@brief handles a keypress that has occurred
+    ///@param event the QKeyEvent that occurred
     void keyPressEvent(QKeyEvent *event) override;
+
+    ///@brief handles resizes when needed
+    ///@param event the QResizeEvent that has occurred
     void resizeEvent(QResizeEvent *event) override;
 
 public:
+    ///@brief constructs a view object
+    ///@param model the model for the game
     View(Model &model, QWidget *parent = nullptr);
+
+    ///@brief deconstructor
     ~View();
 
 signals:
+    ///@brief signals that a key has been pressed
+    ///@param key the key pressed
     void keyPressed(KeyStroke key);
 
 public slots:
+    ///@brief updates the view on each frame
+    ///@param frame the frame to be set to
     void updateFrame(QPixmap frame);
+
+    ///@brief loops the audio played
     void loopAudio();
+
+    ///@brief plays the sound effect passed
+    ///@param sound the sound to be played
     void playSoundEffect(SoundEffect sound);
 
 private:
+    ///@brief pointer to the Ui
     Ui::View *ui;
 
+    ///@brief the size of the view
     QSize gameSize;
 
+    ///@brief the media player to be sent to output
     QMediaPlayer *music;
+
+    ///@brief the player that actually outputs media
     QAudioOutput *output;
 
+    ///sound effects used in game, explained by name
     QSoundEffect* dig;
-
     QSoundEffect* walk;
     QSoundEffect* findBone;
     QSoundEffect* door;
@@ -57,6 +78,7 @@ private:
     QSoundEffect* wrong;
     QSoundEffect* victory;
 
+    ///@brief map holding the sound effects
     QMap<SoundEffect, QSoundEffect*> soundEffects;
 };
 #endif // VIEW_H
